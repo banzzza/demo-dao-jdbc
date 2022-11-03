bf6559a5211f8b4c7e2c6a493f672636dc66f154
Bernardo Zardo Mergen <bernardozardomergen@gmail.com>
2022-11-03 09:36:12 -0300

    Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,14 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 4: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update Completed.");
+		
+		
 	}
 
 }
